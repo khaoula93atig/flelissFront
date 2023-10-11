848f7c662bc90e54e16a043820361f24c519b854
unknown <khaoulatig@gmail.com>
2023-10-11 17:00:27 +0100

    report visit audit

--- src/app/visit-audits/chick-reception-list/chick-reception-list.component.ts
+++ src/app/visit-audits/chick-reception-list/chick-reception-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { ClrDatagrid } from '@clr/angular';
-import { FlockService } from 'src/app/services/flock.service';
-import { HouseService } from 'src/app/services/house.service';
-import { VisitAuditsService } from 'src/app/services/visit-audits.service';
-import { AutofocusDirective } from 'src/app/shared/autofocus.directive';
-import html2canvas from 'html2canvas';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {ClrDatagrid} from '@clr/angular';
+import {FlockService} from 'src/app/services/flock.service';
+import {HouseService} from 'src/app/services/house.service';
+import {VisitAuditsService} from 'src/app/services/visit-audits.service';
+import {AutofocusDirective} from 'src/app/shared/autofocus.directive';
+import {getBase64ImageFromURL} from '../../shared/ImageFromUrl';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import jsPDF from 'jspdf';
 
 @Component({
@@ -13,97 +17,160 @@ import jsPDF from 'jspdf';
   styleUrls: ['./chick-reception-list.component.css']
 })
 export class ChickReceptionListComponent implements OnInit {
-  show = false
-  farmId =''
-  visits=[]
-  loading = true 
-  house:any
-  @ViewChild(ClrDatagrid) dg: ClrDatagrid
-  constructor( private visitAuditsService : VisitAuditsService,
-    private houseService : HouseService,
-    private flockService : FlockService) { }
+  show = false;
+  farmId = '';
+  visits = [];
+  loading = true;
+  house: any;
+  detail: any;
+  variation: any;
+  @ViewChild(ClrDatagrid) dg: ClrDatagrid;
+
+  constructor(private visitAuditsService: VisitAuditsService,
+              private houseService: HouseService,
+              private flockService: FlockService) {
+  }
 
   ngOnInit(): void {
     this.farmId = localStorage.getItem('farmID');
-    this.visitAuditsService.getCheckReceptionByFarm(this.farmId).subscribe(data=>{//console.log(data)
-      this.visits=data
-      /*let res:any[];
-      res=data;
-      
-      {res.map((i)=>{this.houseService.gethouse(i.housesId).subscribe(data1=>{
-        i.house=data1 ;
-         });
-          this.flockService.findById(i.flockId).subscribe(data2=>{i.flock=data2})
-         return i ;
-      });
-      this.visits=res
-      }
-      console.log(this.visits)*/
-  })
-    this.loading = false
+    this.visitAuditsService.getCheckReceptionByFarm(this.farmId).subscribe(data => {
+      this.visits = data;
+    });
+    this.loading = false;
   }
-  @ViewChild(AutofocusDirective) autofocus: AutofocusDirective
+
+  @ViewChild(AutofocusDirective) autofocus: AutofocusDirective;
   @Output() refreshList = new EventEmitter();
 
- 
 
   open() {
-    this.show = true
+    this.show = true;
     // clone the user (we don't want to modify the original in the dialog)
-    console.log('ok')
-    /*let res:any[];
-      res=this.visits;
-      
-      {res.map((i)=>{this.houseService.gethouse(i.housesId).subscribe(data1=>{
-        i.house=data1 ;
-         });
-          this.flockService.findById(i.flockId).subscribe(data2=>{i.flock=data2})
-         return i ;
-      });
-      this.visits=res
-      }
-      console.log(this.visits)*/
+    console.log('ok');
     setTimeout(() => {
       if (this.autofocus) {
-        this.autofocus.setFocus()
+        this.autofocus.setFocus();
       }
-    }, 0.1)
+    }, 0.1);
   }
 
   close() {
-    this.show = false
+    this.show = false;
   }
 
-  getHouseById(event){
-    console.log(event)
-    this.houseService.gethouse(event).subscribe(data=>{
-this.house=data
-    })
-  }
   onDetailOpen(event) {
-    console.log(event)
-    /*this.houseService.gethouse(event.housesId).subscribe(data=>{
-      this.house=data})
-    if (event != null && event != undefined) {
-      this.getHouseById(event)
-      /*this.houseService.gethouse(event.housesId).subscribe(data=>{
-        console.log(data)
-      this.house=data})
-    }*/
+    console.log(event);
+    this.detail = event;
   }
-  generatePdf(data) {
-    html2canvas(data, { allowTaint: true }).then((canvas) => {
-      let HTML_Width = canvas.width
-      let HTML_Height = canvas.height
-      let top_left_margin = 15
-      let PDF_Width = HTML_Width + top_left_margin * 2
-      let PDF_Height = PDF_Width * 1.5 + top_left_margin * 2
-      let canvas_image_width = HTML_Width
-      let canvas_image_height = HTML_Height
-      let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1
-      canvas.getContext('2d')
-      let imgData = canvas.toDataURL('image/jpeg', 1.0)
-      let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height])
+
+  public async export(): Promise<void> {
+  if (this.detail.totalScore >= 40) {
+    this.variation = 'Excellent' ;
+  }else if (this.detail.totalScore >= 20 && this.detail.totalScore <= 39){
+    this.variation = 'Acceptable';
+  }
+  else if (this.detail.totalScore <= 19){
+  this.variation = 'Bad';
+  }
+    const backgroundImage = await getBase64ImageFromURL('/assets/fleliss-v-negatif.png');
+    const docDefinition = {
+      background: {
+        image: backgroundImage,
+        width: 595,
+        opacity: 0.08,
+        verticalAlign: 'middle',
+        margin: [0, 180, 0, 0],
+        style: 'pageBackground',
+      },
+      content: [
+        {text: 'Chick Reception list', style: 'header'},
+        {text: 'Visit date : ' + this.detail.visitDate, style: 'subheader'},
+        {
+          style: 'tableHeader',
+          table: {
+            headerRows: 1,
+            body: [
+              ['House :', this.detail.house.houseName, 'Flock :', this.detail.flock.flockName],
+              ['Breed :', this.detail.breed, 'Hatch date :', this.detail.hatchDate],
+              ['ps Age :', this.detail.psAge, 'ps Origin :', this.detail.psOrigin]
+            ],
+          },
+          layout: 'noBorders',
+        },
+        {text: 'Total score : ' + this.detail.totalScore + ' ' + this.variation, style: 'subheader'},
+        {
+          style: 'tableContent',
+          table: {
+            headerRows: 1,
+            body: [
+              ['', '', 'Score'],
+              [{text: 'Navel', rowSpan: 3}, 'clean and well healed', this.detail.navelCleanWellHealed],
+              ['', 'Closed but slight abrasiveness', this.detail.navelClosedSlightAbrasiveness],
+              ['', 'Not closed/ strung/ button attached or disclored', this.detail.navelNotclosedStrungButton],
+              [{text: 'Legs', rowSpan: 3}, 'clean , waxy legs', this.detail.legsCleanWaxy],
+              ['', 'Some dryness / pale', this.detail.legsDrynessPale],
+              ['', 'Deshydrated with vein protruding', this.detail.legsDeshydratedVienProtruding],
+              [{text: 'Hocks', rowSpan: 3}, 'Clean , no blemishes', this.detail.hocksCleanNoblemishes],
+              ['', 'Slight blushing', this.detail.hocksSlightBlushing],
+              ['', 'Red color / heavy blushing', this.detail.hocksRedcolorHeavyblushing],
+              [{text: 'Defects', rowSpan: 3}, 'clean , no defects', this.detail.defectsCleanNodefects],
+              ['', 'Minor defects (yolk staining , feather coloration, ...)', this.detail.defectsMinorDefects],
+              ['', 'missing eye / blind , legs with cuts/ abrasions , spraddled legs ,cross beaks , poor feathering , clubbed down', this.detail.defectsEyeLegsSpraddled]
+            ],
+          }
+        }
+      ],
+
+      styles: {
+        pageBackground: {
+          alignment: 'center',
+          verticalAlign: 'middle',
+          textAlign: 'center',
+          display: 'flex'
+        },
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+          alignment: 'center',
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 10, 0, 5]
+        },
+        tableHeader: {
+          margin: [0, 5, 0, 15],
+          layout: 'noBorders',
+          fontSize: 14,
+          bold: true,
+        },
+        tableContent: {
+          margin: [0, 5, 0, 15]
+        },
+        head: {
+          bold: true, fillColor: '#EEEEEE'
+        }
+      }
+    };
+
+    pdfMake.createPdf(docDefinition).download('Report-CR-' + this.detail.visitDate + '.pdf');
+  }
+
+
+  /*generatePdf(data) {
+    html2canvas(data, {allowTaint: true}).then((canvas) => {
+      let HTML_Width = canvas.width;
+      let HTML_Height = canvas.height;
+      let top_left_margin = 15;
+      let PDF_Width = HTML_Width + top_left_margin * 2;
+      let PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
+      let canvas_image_width = HTML_Width;
+      let canvas_image_height = HTML_Height;
+      let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+      canvas.getContext('2d');
+      let imgData = canvas.toDataURL('image/jpeg', 1.0);
+      let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
       pdf.addImage(
         imgData,
         'JPG',
@@ -111,9 +178,9 @@ this.house=data
         top_left_margin,
         canvas_image_width,
         canvas_image_height,
-      )
+      );
       for (let i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage([PDF_Width, PDF_Height], 'p')
+        pdf.addPage([PDF_Width, PDF_Height], 'p');
         pdf.addImage(
           imgData,
           'JPG',
@@ -121,9 +188,9 @@ this.house=data
           -(PDF_Height * i) + top_left_margin * 4,
           canvas_image_width,
           canvas_image_height,
-        )
+        );
       }
-      pdf.save('Report-chick-Reception.pdf')
-    })
-  }
+      pdf.save('Report-chick-Reception.pdf');
+    });
+  }*/
 }
--- src/app/visit-audits/list-brooding-check-list/list-brooding-check-list.component.ts
+++ src/app/visit-audits/list-brooding-check-list/list-brooding-check-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { FarmService } from 'src/app/services/farm.service';
-import { HouseService } from 'src/app/services/house.service';
-import { VisitAuditsService } from 'src/app/services/visit-audits.service';
-import { AutofocusDirective } from 'src/app/shared/autofocus.directive';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {FarmService} from 'src/app/services/farm.service';
+import {HouseService} from 'src/app/services/house.service';
+import {VisitAuditsService} from 'src/app/services/visit-audits.service';
+import {AutofocusDirective} from 'src/app/shared/autofocus.directive';
+import {getBase64ImageFromURL} from '../../shared/ImageFromUrl';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
@@ -15,99 +17,128 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 })
 export class ListBroodingCheckListComponent implements OnInit {
 
-  
-  @ViewChild(AutofocusDirective) autofocus: AutofocusDirective
-  @Output() refreshList = new EventEmitter();
-  show = false
-  farmId =''
-  visits=[]
-  loading = true 
-  breedingDetail :any
 
-  constructor(private visitAuditsService : VisitAuditsService,
-    private houseService : HouseService,
-    private farmService: FarmService) { }
+  @ViewChild(AutofocusDirective) autofocus: AutofocusDirective;
+  @Output() refreshList = new EventEmitter();
+  show = false;
+  farmId = '';
+  visits = [];
+  loading = true;
+  breedingDetail: any;
+
+  constructor(private visitAuditsService: VisitAuditsService,
+              private houseService: HouseService,
+              private farmService: FarmService) {
+  }
 
   ngOnInit(): void {
     this.farmId = localStorage.getItem('farmID');
-    this.visitAuditsService.getBroodingCheckByFarm(this.farmId).subscribe(data=>{console.log(data)
-      this.visits=data
-    })
-  
-    this.loading = false
+    this.visitAuditsService.getBroodingCheckByFarm(this.farmId).subscribe(data => {
+      console.log(data);
+      this.visits = data;
+    });
+
+    this.loading = false;
   }
+
   open() {
-    this.show = true
-    console.log('ok')
+    this.show = true;
+    console.log('ok');
     setTimeout(() => {
       if (this.autofocus) {
-        this.autofocus.setFocus()
+        this.autofocus.setFocus();
       }
-    }, 0.1)
+    }, 0.1);
   }
 
 
-  public export(): void {
-
+  public async export(): Promise<void>{
+    const backgroundImage = await getBase64ImageFromURL('/assets/fleliss-v-negatif.png');
     const docDefinition = {
-    
+      background: {
+        image: backgroundImage,
+        width: 595,
+        opacity: 0.08,
+        verticalAlign: 'middle',
+        margin: [0, 180, 0, 0],
+        style: 'pageBackground',
+      },
       content: [
-        { text: "Brooding check list", style: "header" },
-        { text: "Visit date : "+this.breedingDetail.creationDate, style: "subheader" },
+        {text: 'Brooding check list', style: 'header'},
+        {text: 'Visit date : ' + this.breedingDetail.creationDate, style: 'subheader'},
         {
           text:
-            "Center : "+this.breedingDetail.center.centerName,
-          style: "subheader"
+            'Center : ' + this.breedingDetail.center.centerName,
+          style: 'subheader'
         },
         {
           text:
-            "House : "+this.breedingDetail.house.houseName,
-          style: "subheader"
+            'House : ' + this.breedingDetail.house.houseName,
+          style: 'subheader'
         },
         {
-          style: "tableExample",
-          table: {headerRows:1,
-            body: [["", "", "Status"],
-              [{text:"Before Arrival",bold: true,rowSpan:15}, "Litter depth : (2-4 cm)", this.breedingDetail.litterDepth],
-              ["","litter temperature : (28-30°C)", this.breedingDetail.temperature],
-              ["", "Pre-heat at least : 24 h ( 48h in winter )", this.breedingDetail.preHeatLeast],
-              ["", "Air temperature : (30°C/86°F)", this.breedingDetail.airTemperature],
-              ["", "Floor temperature : (28-30°C)", this.breedingDetail.floorTemperature],
-              ["", "Spot brooding: (32°C under brooder / 29° at brooderedge)", this.breedingDetail.spotBrooding],
-              ["", "Realtive humidity: (60-70%)", this.breedingDetail.realtiveHumidity],
-              ["", "Distance to access water: (No more than 1meter/3.3 feet)", this.breedingDetail.distanceAccessWater],
-              ["", "Air speed : (<0,15 m/s) ", this.breedingDetail.airSpeed],
-              ["", "Check feed form: (Crumble / pellet 2mm)", this.breedingDetail.checkFeed],
-              ["", "Feed on paper : (80% of brooding area)", this.breedingDetail.feedOnPaper],
-              ["", "Feeders Trays : (1/100 chicks)", this.breedingDetail.feedersTrays],
-              ["", "Drinkers Nipple lines : (12 birds/nipple)", this.breedingDetail.drinkersNippleLines],
-              ["", "Drinkers (bell drinkers) : (6/1000 birds)", this.breedingDetail.drinkersBell],
-              ["", "Drinkers supplementary : (10/1000 chicks)", this.breedingDetail.drinkersSupp],
-              [{text:"Check delivery", bold: true,rowSpan:7}, "Delivery vehicle temperature (chick box :32°C outside chickbox : 24°C)", this.breedingDetail.deliveryVehiTemp],
-              ["", "Delivery vehicle humidity: (60-70%)", this.breedingDetail.deliveryVehiHumi],
-              ["", "Chick confort ( rectal temperature ):(39.5-40.5 °C)", this.breedingDetail.deliveryVehiTemp],
-              ["", "External environmental conditions ( Temperature , Humidity )", this.breedingDetail.externalEnvCond],
-              ["", "Transit time ( hatchery- farm ): (short / long)", this.breedingDetail.transitTime],
-              ["", "Delivery vehicle : air exchange", this.breedingDetail.deliveryVehicleAirExch],
-              ["", "Delivery vehicle : hygiene (disinfected)", this.breedingDetail.deliveryVehicleHygiene],
-              [{text:"The arrival",bold: true,rowSpan:5}, "Internal chick temperature : (40°C-41°C)", this.breedingDetail.internalChickTemp],
-              ["", "Weigh a sample of chick record average live weight and CV% /uniformity : (Average 36-45g)", this.breedingDetail.weighSampleChick],
-              ["", "Light intensity :(30-40 lux)", this.breedingDetail.lightIntensity],
-              ["", "Ensure feed and water is available", this.breedingDetail.ensureFeedWater],
-              ["", "Water temperature :(18-21 °C)", this.breedingDetail.waterTemperature],
-              [{text:"After installation",bold: true,rowSpan:4}, "Check chick behavior after 1-2 hours", this.breedingDetail.checkChickBehavior],
-              ["", "Check chick water supply ( fresh & clean water )", this.breedingDetail.checkChickWaterSupply],
-              ["", "Check feed supply ( small & frequent )", this.breedingDetail.checkFeedSupply],
-              ["", "Check chick crop fill", this.breedingDetail.checkChickCropFill],
-              [{text:"",rowSpan:3}, "Distribution of day old chicks", this.breedingDetail.distributionDayOldChicks],
-              ["", "Daily purges piping", this.breedingDetail.dailyPurgesPiping],
-              ["", "Air quality", this.breedingDetail.airQuality],
+          style: 'tableExample',
+          table: {
+            headerRows: 1,
+            body: [['', '', 'Status'],
+              [{text: 'Before Arrival', bold: true, rowSpan: 15}, 'Litter depth : (2-4 cm)', this.breedingDetail.litterDepth],
+              ['', 'litter temperature : (28-30°C)', this.breedingDetail.temperature],
+              ['', 'Pre-heat at least : 24 h ( 48h in winter )', this.breedingDetail.preHeatLeast],
+              ['', 'Air temperature : (30°C/86°F)', this.breedingDetail.airTemperature],
+              ['', 'Floor temperature : (28-30°C)', this.breedingDetail.floorTemperature],
+              ['', 'Spot brooding: (32°C under brooder / 29° at brooderedge)', this.breedingDetail.spotBrooding],
+              ['', 'Realtive humidity: (60-70%)', this.breedingDetail.realtiveHumidity],
+              ['', 'Distance to access water: (No more than 1meter/3.3 feet)', this.breedingDetail.distanceAccessWater],
+              ['', 'Air speed : (<0,15 m/s) ', this.breedingDetail.airSpeed],
+              ['', 'Check feed form: (Crumble / pellet 2mm)', this.breedingDetail.checkFeed],
+              ['', 'Feed on paper : (80% of brooding area)', this.breedingDetail.feedOnPaper],
+              ['', 'Feeders Trays : (1/100 chicks)', this.breedingDetail.feedersTrays],
+              ['', 'Drinkers Nipple lines : (12 birds/nipple)', this.breedingDetail.drinkersNippleLines],
+              ['', 'Drinkers (bell drinkers) : (6/1000 birds)', this.breedingDetail.drinkersBell],
+              ['', 'Drinkers supplementary : (10/1000 chicks)', this.breedingDetail.drinkersSupp],
+              [{
+                text: 'Check delivery',
+                bold: true,
+                rowSpan: 7
+              }, 'Delivery vehicle temperature (chick box :32°C outside chickbox : 24°C)', this.breedingDetail.deliveryVehiTemp],
+              ['', 'Delivery vehicle humidity: (60-70%)', this.breedingDetail.deliveryVehiHumi],
+              ['', 'Chick confort ( rectal temperature ):(39.5-40.5 °C)', this.breedingDetail.deliveryVehiTemp],
+              ['', 'External environmental conditions ( Temperature , Humidity )', this.breedingDetail.externalEnvCond],
+              ['', 'Transit time ( hatchery- farm ): (short / long)', this.breedingDetail.transitTime],
+              ['', 'Delivery vehicle : air exchange', this.breedingDetail.deliveryVehicleAirExch],
+              ['', 'Delivery vehicle : hygiene (disinfected)', this.breedingDetail.deliveryVehicleHygiene],
+              [{
+                text: 'The arrival',
+                bold: true,
+                rowSpan: 5
+              }, 'Internal chick temperature : (40°C-41°C)', this.breedingDetail.internalChickTemp],
+              ['', 'Weigh a sample of chick record average live weight and CV% /uniformity : (Average 36-45g)', this.breedingDetail.weighSampleChick],
+              ['', 'Light intensity :(30-40 lux)', this.breedingDetail.lightIntensity],
+              ['', 'Ensure feed and water is available', this.breedingDetail.ensureFeedWater],
+              ['', 'Water temperature :(18-21 °C)', this.breedingDetail.waterTemperature],
+              [{
+                text: 'After installation',
+                bold: true,
+                rowSpan: 4
+              }, 'Check chick behavior after 1-2 hours', this.breedingDetail.checkChickBehavior],
+              ['', 'Check chick water supply ( fresh & clean water )', this.breedingDetail.checkChickWaterSupply],
+              ['', 'Check feed supply ( small & frequent )', this.breedingDetail.checkFeedSupply],
+              ['', 'Check chick crop fill', this.breedingDetail.checkChickCropFill],
+              [{text: '', rowSpan: 3}, 'Distribution of day old chicks', this.breedingDetail.distributionDayOldChicks],
+              ['', 'Daily purges piping', this.breedingDetail.dailyPurgesPiping],
+              ['', 'Air quality', this.breedingDetail.airQuality],
             ]
           }
         }
       ],
 
       styles: {
+        pageBackground: {
+          alignment: 'center',
+          verticalAlign: 'middle',
+          textAlign: 'center',
+          display: 'flex'
+        },
         header: {
           fontSize: 18,
           bold: true,
@@ -122,20 +153,19 @@ export class ListBroodingCheckListComponent implements OnInit {
         tableExample: {
           margin: [0, 5, 0, 15]
         },
-        head:{
-          bold:true, fillColor:'#EEEEEE'
+        head: {
+          bold: true, fillColor: '#EEEEEE'
         }
       }
     };
 
-    pdfMake.createPdf(docDefinition).download("test.pdf");
+    pdfMake.createPdf(docDefinition).download('report-BC-' + this.breedingDetail.creationDate + '.pdf');
   }
 
   onDetailOpen(event) {
-    console.log(event)
-    this.breedingDetail=event
+    console.log(event);
+    this.breedingDetail = event;
   }
-  
-   
+
 
 }
